80af96aeff139669327f24cd0c91874277cf56c3
Unknown <wahlquisteric@gmail.com>
2017-08-15 17:26:02 -0700

    6th Commit
    -Added comments for Player class

--- src/Player.java
+++ src/Player.java
@@ -1,34 +1,44 @@
-
+/*
+ * This class represents a Player object in the game. It keeps track of a single contestant, including the name of the
+ * contestant and how many games they have won.
+ */
 public class Player {
 
 	private String name;
 	private int wins;
 	
-	public Player() {
+	//Explicit private default constructor that prevents empty Players from being created
+	private Player() {
 		this("", 0);
 	}
 	
+	//Main constructor used for creating Player objects
 	public Player(String name, int wins) {
 		this.name = name;
 		this.wins = 0;
 	}
 	
+	//Returns the name of this Player
 	public String getName() {
 		return name;
 	}
 	
+	//Returns the win count of this Player
 	public int getWins() {
 		return wins;
 	}
 	
+	//Sets the name of this Player
 	public void setName(String name) {
 		this.name = name;
 	}
 	
-	public void setWins(int wins) {
-		this.wins = wins;
+	//Increments the win count of this Player by 1
+	public void addWin() {
+		wins++;
 	}
 	
+	//Returns a string that displays this Player's name as well as win count
 	public String toString() {
 		return name + ": " + wins + " wins";
 	}
