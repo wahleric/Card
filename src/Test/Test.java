ec307f1ea46169e6e64d554ed8ce8240503bbcd7
Eric Wahlquist <wahlquisteric@gmail.com>
2017-12-21 07:22:40 -0800

    Beginning test file

---
+++ src/Test/Test.java
@@ -0,0 +1,25 @@
+package Test;
+import java.io.*;
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	// Tests Player class
+	
+	// Tests CardGenerator class
+	
+	// Tests Card class
+	
+	// Tests Deck class
+	
+	// Tests Board class
+	
+	// Tests ColdZone class
+	
+	// Tests HotZone class
+
+}
